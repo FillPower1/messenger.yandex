355d728120edb576492d12767096e17690852488
FillPower1 <sashabazarbaev@mail.ru>
2021-03-27 16:10:12 +0300

    fix redirect

--- src/pages/profile/personal-data/personal-data.ts
+++ src/pages/profile/personal-data/personal-data.ts
@@ -2,8 +2,6 @@ import { Block } from '../../../core/block/index.js'
 import { renderChild } from '../../../utils/render.js'
 import { ProfileButton } from '../../../components/profile-button/index.js'
 import { Avatar } from '../../../components/avatar/avatar.js'
-import { Router } from '../../../core/router/router.js'
-import { Routes } from '../../../constants.js'
 
 import { Info } from './info/index.js'
 import { UploadAvatar } from './upload-avatar/index.js'
@@ -14,26 +12,11 @@ export class PersonalData extends Block {
     constructor() {
         super('div', {
             className: PersonalData.className,
-            components: [
-                new ProfileButton(),
-                new Avatar(),
-                new Info(),
-                new UploadAvatar()
-            ]
+            components: [new ProfileButton(), new Avatar(), new Info(), new UploadAvatar()]
         })
     }
 
     componentDidRender() {
         renderChild(this.element, this.props.components)
-    }
-
-    render() {
-        // редирект, если нет авторизации
-        if (!this.state.get('auth.isAuthorized', null)) {
-            new Router().go(Routes.Login)
-            this.hide()
-        }
-
-        return ''
     }
 }
